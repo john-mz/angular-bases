9563d9ee4a34d5cd21f1ba4f2104fccd58d4522b
Stiven <johnstiven40@gmail.com>
2025-06-15 19:55:15 -0500

    test

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent, ListComponent],
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
   exports: [MainPageComponent],
   imports: [
     CommonModule
